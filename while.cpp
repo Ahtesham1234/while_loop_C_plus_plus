db0e84c1ded1009e4e8661ec5bf63265a3b433d3
Ahtesham Ul Haq(@hptouchpad519) <hptouchpad519@gmail.com>
2017-02-11 17:53:05 +0500

    Create while.cpp
    this a relaxing and very fun program

---
+++ while.cpp
@@ -0,0 +1,16 @@
+//while.cpp
+// demonstrates WHILE loop in program
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int n= 99;    // remember that n doesn't initialized to 0
+
+while(n != 0 )   // loop until n is 0
+cin>> n ;         //read a number into n
+
+cout<< endl;
+return 0;
+}
